3e82d15e19f668808223ee7052726cff88998821
LuxITD <luismardur@gmail.com>
2024-05-23 11:54:30 -0500

    cambio usuarios

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <div class="mb-4">
                     <a href="{{ route('students.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Create
+                        New Student
                     </a>
                 </div>
 
@@ -33,7 +33,7 @@
                             <td class="border py-2 px-4 text-center">
                                 <div class="flex justify-center">
                                     <a href="{{ route('students.edit', $student->id) }}" class="bg-violet-500 dark:dg-violet-700 hover:bg-violet-600 dark:hover:bg-violet-800 py-2 px-4 rounded mr-2 text-white rounded" >Edit</a>
-                                    <button type="button" onclick="confirmDelete('{{ $student->id }}')" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:bg-red-600">Eliminar</button>
+                                    <button type="button" onclick="confirmDelete('{{ $student->id }}')" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:bg-red-600">Delete</button>
                                 </div>
 
 
